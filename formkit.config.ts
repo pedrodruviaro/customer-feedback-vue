fa4d8e6faaad7e1800ba8bd1265ddec1a532be92
pedrodruviaro <pedrodruviaro@gmail.com>
2024-02-02 15:07:04 -0300

    rmv: pt lang formKit

--- formkit.config.ts
+++ formkit.config.ts
@@ -1,9 +1,7 @@
 import { defaultConfig } from '@formkit/vue'
 import { rootClasses } from './formkit.theme'
-import { pt } from '@formkit/i18n'
 
 export default defaultConfig({
-  locale: 'pt',
   config: {
     rootClasses
   }
