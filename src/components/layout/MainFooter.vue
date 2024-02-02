7c674d6f0143afcc9b116d15bc59518447e82de4
pedrodruviaro <pedrodruviaro@gmail.com>
2024-02-02 15:07:26 -0300

    refactor: footer text content

--- src/components/layout/MainFooter.vue
+++ src/components/layout/MainFooter.vue
@@ -1,5 +1,11 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const currentYear = computed(() => new Date().getFullYear())
+</script>
+
 <template>
   <footer class="wrapper text-center py-8">
-    <p class="text-sm">&copy; Copyright</p>
+    <p class="text-sm">Feedbacker - {{ currentYear }}</p>
   </footer>
 </template>
