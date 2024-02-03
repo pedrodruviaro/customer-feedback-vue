a7599096442e43d6db76b9ce23677c71ce31e831
pedrodruviaro <pedrodruviaro@gmail.com>
2024-02-03 09:30:33 -0300

    add: RoadmapItem

---
+++ src/components/roadmap/RoadmapItem.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { PhArrowSquareIn } from '@phosphor-icons/vue'
+import { type Task } from '@/types'
+
+interface Props {
+  task: Task
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <li>
+    <RouterLink
+      :to="{ name: 'KaizenTask', params: { id: task.id } }"
+      class="font-semibold flex items-start gap-2"
+    >
+      <PhArrowSquareIn class="flex-0 relative top-1" />
+      <p class="flex-1 text-sm">{{ task.title }}</p>
+    </RouterLink>
+  </li>
+</template>
