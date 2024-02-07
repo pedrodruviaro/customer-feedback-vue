12207159c1eaae70c13f5225f189962f85b8ce59
pedrodruviaro <pedrodruviaro@gmail.com>
2024-02-07 10:55:08 -0300

    add: logout button

--- src/components/base/BaseProfileIcon.vue
+++ src/components/base/BaseProfileIcon.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import { computed } from 'vue'
+import { useAuthActions } from '@/composables/useAuthActions'
+import { PhSignOut } from '@phosphor-icons/vue'
+import { ref } from 'vue'
 
 interface Props {
   label: string
@@ -10,12 +13,40 @@ const props = defineProps<Props>()
 const firstLetter = computed(() => {
   return props.label?.charAt(0).toUpperCase()
 })
+
+const { logout } = useAuthActions()
+
+const isOptionsActive = ref(false)
 </script>
 
 <template>
-  <span
-    class="bg-red-200 text-red-600 text-lg font-bold grid place-items-center w-10 h-10 cursor-pointer rounded-full"
-  >
-    {{ firstLetter }}
-  </span>
+  <div class="relative">
+    <button
+      @click="isOptionsActive = !isOptionsActive"
+      class="bg-red-200 text-red-600 text-lg font-bold grid place-items-center w-10 h-10 cursor-pointer rounded-full"
+    >
+      {{ firstLetter }}
+    </button>
+
+    <div class="rounded shadow-md absolute right-0">
+      <Transition>
+        <button class="flex items-center gap-2 p-2" v-if="isOptionsActive" @click="logout">
+          <PhSignOut />
+          Logout
+        </button>
+      </Transition>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.1s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
--- src/components/layout/MainHeader.vue
+++ src/components/layout/MainHeader.vue
@@ -1,14 +1,10 @@
 <script setup lang="ts">
 import MainLogo from '@/components/layout/MainLogo.vue'
 import MainNavigation from '@/components/layout/MainNavigation.vue'
-import BaseButton from '@/components/base/BaseButton.vue'
 import BaseProfileIconVue from '@/components/base/BaseProfileIcon.vue'
-import { useAuthActions } from '@/composables/useAuthActions'
 import { useCurrentUser } from 'vuefire'
 
 const user = useCurrentUser()
-
-const { logout } = useAuthActions()
 </script>
 
 <template>
@@ -16,7 +12,6 @@ const { logout } = useAuthActions()
     <div class="wrapper">
       <div class="flex justify-between items-center gap-4 flex-wrap">
         <MainLogo />
-        <!-- <button @click="logout">Logout</button> -->
 
         <div class="flex items-center gap-4">
           <BaseProfileIconVue :label="String(user?.displayName)" />
